408afdce44101ee3b80bbf733276df81cdc68df2
Bobylev Vladimir <v.bobylev@rambler-co.ru>
2024-07-13 20:42:31 +0300

    readonly

--- app.js
+++ app.js
@@ -1,8 +1,31 @@
 "use strict";
 // const a = 1;
+// HACK: Function
 function getFullName(firstName, lastName) {
     return `${firstName} ${lastName}`;
 }
 const getFullNameArrow = (firstName, lastName) => {
     return `${firstName} ${lastName}`;
 };
+// HACK: Array
+const skills = ['Dev', 'DevOps'];
+const skillsAny = ['Dev', 'DevOps', 1, { a: 1 }]; // bad way
+// HACK: Tuple (кортеж)
+// массив ограниченной длины.
+// В [] точно (буквально) описана структура.
+// Фикс длина, даже после удаления и добавления элемнетов
+const skill = [1, 'Dev'];
+// const id = skill[0]
+// const skillName = skill[1]
+const [id, skillName] = skill;
+// кортеж произвольной длины
+const skillsArr = [1, 'Dev', true, false, true];
+// HACK: Readonly
+// tuples
+const skillRO = [1, 'Dev'];
+// skillRO[0] = [9] // недопустимо изменять readonly
+const skillsRO = ['dev', 'devops'];
+// skillRO[0] = ['str'] // недопустимо изменять readonly
+// arrays
+const skillsGArr = ['q', 'w', 'qwe']; // Array<string> - generyc
+const skillsGArrRO = ['q', 'w', 'qwe']; // аналог readonly string[]
--- app.ts
+++ app.ts
@@ -1,5 +1,6 @@
 // const a = 1;
 
+// HACK: Function
 function getFullName(firstName: string, lastName: string): string {
     return `${firstName} ${lastName}`
 }
@@ -8,3 +9,37 @@ const getFullNameArrow = (firstName: string, lastName: string): string => {
     return `${firstName} ${lastName}`
 }
 
+
+// HACK: Array
+const skills: string[] = ['Dev', 'DevOps']
+
+const skillsAny: any[] = ['Dev', 'DevOps', 1, { a: 1 }] // bad way
+
+// HACK: Tuple (кортеж)
+
+// массив ограниченной длины.
+// В [] точно (буквально) описана структура.
+// Фикс длина, даже после удаления и добавления элемнетов
+const skill: [number, string] = [1, 'Dev']
+
+// const id = skill[0]
+// const skillName = skill[1]
+
+const [id, skillName] = skill
+
+// кортеж произвольной длины
+const skillsArr: [number, string, ...boolean[]] = [1, 'Dev', true, false, true]
+
+
+
+// HACK: Readonly
+// tuples
+const skillRO: readonly [number, string] = [1, 'Dev']
+// skillRO[0] = [9] // недопустимо изменять readonly
+
+const skillsRO: readonly string[] = ['dev', 'devops']
+// skillRO[0] = ['str'] // недопустимо изменять readonly
+
+// arrays
+const skillsGArr: Array<string> = ['q', 'w', 'qwe'] // Array<string> - generyc
+const skillsGArrRO: ReadonlyArray<string> = ['q', 'w', 'qwe'] // аналог readonly string[]
